66528596db970a4d6f59e88a5807a74829612252
Alexandru Ica Gabriel <alexandruica703@gmail.com>
2020-05-06 14:18:38 +0300

    Implement radix sort

--- src/Sortari_Cautare_SDO/radix_sort.cpp
+++ src/Sortari_Cautare_SDO/radix_sort.cpp
@@ -2,50 +2,81 @@
 #include <array>
 #include <cstddef>
 #include <cstdlib>
+#include <deque>
 #include <fstream>
 #include <iostream>
-#include <queue>
 #include <type_traits>
 #include <utility>
 #include <vector>
 
-int max{ 0 };
-
-auto generate_input(int const n, int const a, int const b, int const c)
-    -> std::vector<int>
+auto generate_input(std::uint32_t const n,
+                    std::uint32_t const a,
+                    std::uint32_t const b,
+                    std::uint32_t const c) -> std::vector<std::uint32_t>
 {
-    std::vector<int> result(static_cast<std::size_t>(n + 1), 0);
+    std::vector<std::uint32_t> result(n, 0);
 
-    max = result[1] = b;
+    result[0] = b % c;
 
-    for(std::size_t i = 2; i < result.size(); ++i) {
-        std::int64_t prod = static_cast<std::int64_t>(a) * result[i - 1] + b;
-        result[i] = static_cast<int>(prod % c);
-        max = std::max(max, result[i]);
+    for(std::uint32_t i = 1; i < n; ++i) {
+        std::uint32_t prod =
+            (static_cast<std::uint64_t>(a) * result[i - 1]) % c + b;
+        result[i] = prod % c;
     }
 
     return result;
 }
 
-template<int Base>
-auto radix_sort(std::vector<int>& v) -> void
+constexpr auto nth_byte(std::uint32_t const x, std::uint32_t const n)
+    -> std::uint32_t
 {
-    std::array<std::queue<int>, Base> digits;
-    int pow{ 1 };
+    return (x >> (n * 8)) & 0xFF;
+}
 
-    while(max / pow > 0) {
-        for(int const num : v) {
-            digits[(num / pow) % Base].push(num);
-        }
+auto count_sort(std::vector<std::uint32_t> const& input,
+                std::vector<std::uint32_t>& tmp,
+                std::uint32_t const byte) -> void
+{
+    constexpr std::uint32_t byte_size = 8;
+    constexpr std::uint32_t max_count = 1 << byte_size; // 2 ^ 8
 
-        pow *= Base;
-        v.clear();
+    auto byte_of = [byte](std::uint32_t const num) -> std::uint32_t {
+        return nth_byte(num, byte);
+    };
 
-        for(std::size_t i = 0; i < Base; ++i) {
-            while(!digits[i].empty()) {
-                v.push_back(digits[i].front());
-                digits[i].pop();
-            }
+    std::array<std::uint32_t, max_count> counter{};
+    std::array<std::uint32_t, max_count> idx{};
+
+    counter.fill(0);
+
+    for(auto const elem : input) {
+        ++counter[byte_of(elem)];
+    }
+
+    idx[0] = 0;
+
+    for(std::uint32_t i = 1; i < max_count; ++i) {
+        idx[i] = idx[i - 1] + counter[i - 1];
+    }
+
+    for(auto const elem : input) {
+        tmp[idx[byte_of(elem)]++] = elem;
+    }
+}
+
+auto radix_sort(std::vector<std::uint32_t>& v) -> void
+{
+    std::vector<std::uint32_t> tmp(v.size(), 0);
+    std::uint32_t byte_index{ 0 };
+
+    for(;;) {
+        count_sort(v, tmp, byte_index);
+        count_sort(tmp, v, byte_index + 1);
+        count_sort(v, tmp, byte_index + 2);
+        count_sort(tmp, v, byte_index + 3);
+
+        if((byte_index += 4) >= sizeof(std::uint32_t)) {
+            return;
         }
     }
 }
@@ -53,19 +84,19 @@ auto radix_sort(std::vector<int>& v) -> void
 auto main(int, char*[]) noexcept -> int
 {
     std::ifstream f{ "radixsort.in" };
-    int n{ 0 };
-    int a{ 0 };
-    int b{ 0 };
-    int c{ 0 };
+    std::uint32_t n{ 0 };
+    std::uint32_t a{ 0 };
+    std::uint32_t b{ 0 };
+    std::uint32_t c{ 0 };
 
     f >> n >> a >> b >> c;
 
     auto input = generate_input(n, a, b, c);
-    radix_sort<10>(input);
+    radix_sort(input);
 
     std::ofstream g{ "radixsort.out" };
 
-    for(std::size_t i = 1; i < input.size(); i += 10) {
+    for(std::uint32_t i = 0; i < input.size(); i += 10) {
         g << input[i] << ' ';
     }
 
